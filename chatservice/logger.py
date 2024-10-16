b4c4ea5ee523bd2885e6b21f2ea0a40287c64132
ivanlau <ivanlau91@hotmail.com>
2024-10-16 18:06:19 +0800

    code refactor into chatservice and graylog modules

--- chatservice/logger.py
+++ chatservice/logger.py
@@ -4,7 +4,7 @@ import graypy
 logger = logging.getLogger("rag-logger")
 
 # Graylog server configuration
-handler = graypy.GELFUDPHandler('localhost', 12201)
+handler = graypy.GELFUDPHandler('graylog', 12201)
 
 logger.setLevel(logging.INFO)
 logger.addHandler(handler)
